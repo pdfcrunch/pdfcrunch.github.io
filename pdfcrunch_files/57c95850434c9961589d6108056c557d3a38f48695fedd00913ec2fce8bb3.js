2ea294c6795e5d81fb8f49cbe423e39a04a9a8b3
Juan Uys <opyate@gmail.com>
2023-04-27 21:59:07 +0100

    v2 website

---
+++ pdfcrunch_files/57c95850434c9961589d6108056c557d3a38f48695fedd00913ec2fce8bb3.js
@@ -0,0 +1,1 @@
+!function(){"use strict";function e(){const e=document.getElementById("menuToggle");e&&window.addEventListener("hashchange",(()=>function(e){e.checked=!1}(e)))}"loading"===document.readyState?window.addEventListener("DOMContentLoaded",e):e()}();
